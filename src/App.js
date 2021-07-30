cbb465a6761009b89479f82f954fa46df16ebdbb
molk12 <malloukabac@gmail.com>
2021-07-30 11:08:10 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./App.css";
+import Profil from "./profile";
+//initialisation du state
+class App extends React.Component {
+ constructor (props){
+   super (props) 
+     this.state = {
+     show:false
+          }
+   
+ }
+Toggle =() => {
+  this.setState ({
+  show :!this.state.show
+  } )
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
 }
 
-export default App;
+  render() {
+    return (
+      <div className="descr" >
+       <button onClick={this.Toggle}> SHOW ME PROFIL </button>
+     {
+       this.state.show && <Profil/> 
+     }
+  
+     
+      
+      </div>
+     
+    )
+  }
+} 
+export default App
